bcac8634d9c4dcc2ae8b0967c575a0ded8e4b1b5
Nertil Poci <nertilpoci@hotmail.com>
2022-02-10 22:54:49 +0100

    exercise files for chapter

---
+++ Azure Event Hubs/EventGeneratorLibrary/EventGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventGeneratorLibrary
+{
+    public static class EventGenerator
+    {
+
+        public static IEnumerable<Models.SensorEvent> GetSensorEvents(string sensorType, int events = 50)
+        {
+            int generatedEvents = 0;
+
+            while (generatedEvents < events)
+            {
+                generatedEvents += 1;
+
+                yield return new Models.SensorEvent(sensorType)
+                {
+                    Priority = 1,
+                    AccountId = Guid.NewGuid(),
+                    TimeStamp = DateTime.UtcNow,
+                    SensorMessage="Message received from sensor"
+
+                };
+            }
+        }
+    }
+}
---
+++ Azure Event Hubs/EventGeneratorLibrary/Models/SensorEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventGeneratorLibrary.Models
+{
+    public  class SensorEvent
+    {
+        public SensorEvent(string sensorType)
+        {
+            this.SensorType = sensorType;
+        }
+        public string SensorType { get; private set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public Guid AccountId { get; set; }
+        public int Priority { get; set; }
+        public string SensorMessage { get; set; }
+    }
+}
