eeead19ec7a3a10a4ec149e942ed0067247e941f
Nertil Poci <nertilpoci@hotmail.com>
2022-02-10 22:59:15 +0100

    Update Program.cs

--- Azure Event Hubs/SecurityClient/Program.cs
+++ Azure Event Hubs/SecurityClient/Program.cs
@@ -9,8 +9,9 @@ await StartEventGenerating();
 
 async Task StartEventGenerating()
 {
-    var connectionString = "";
-    var eventHubName = "";
+
+    var connectionString = "Endpoint=sb://eventhubcourse.servicebus.windows.net/;SharedAccessKeyName=sensorloggenerator;SharedAccessKey=Gw54t2gyzr2Ya163Tma8ADbwvlQy2UPkXBMW2oAIblQ=";
+    var eventHubName = "sensordata";
 
     await using (var producer = new EventHubProducerClient(connectionString, eventHubName))
     {
